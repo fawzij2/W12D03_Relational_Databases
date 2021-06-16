89a494102d449ac1c81c328f9eb6a1136500428f
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-16 18:22:41 +0300

    ticket 12 createNewComment

--- routers/controllers/comments.js
+++ routers/controllers/comments.js
@@ -1,24 +1,21 @@
 const commentsModel = require('./../../db/models/comments');
+const connection = require("./../../db/db");
 
 const createNewComment = (req, res) => {
 	const articleId = req.params.id;
 
 	const { comment, commenter } = req.body;
 
-	const newComment = new commentsModel({
-		comment,
-		articleId,
-		commenter,
-	});
+	const query = "INSERT INTO comments (comment, article_id, commenter_id) VALUES (?,?,?)";
+	const commentData = [ comment, articleId, commenter];
 
-	newComment
-		.save()
-		.then((result) => {
-			res.status(201).json(result);
-		})
-		.catch((err) => {
+	connection.query(query,commentData, (err,result)=>{
+		if (err) {
 			res.send(err);
-		});
+			return
+		}
+		res.status(200).json(comment);
+	})
 };
 
 module.exports = {
